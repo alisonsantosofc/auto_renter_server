bb5a127babeca79b4c37728a49eb39e9c6038104
alisonsantosofc <alisonsantoso22@gmail.com>
2022-11-21 18:20:10 -0300

    remove vercel configure

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { PrismaClient } from '@prisma/client';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || process.env.LOCAL_PORT;
+const port = process.env.PORT || process.env.MY_LOCAL_PORT;
 
 const prisma = new PrismaClient({
   log: ['query'],
